daac32db22a073106910126ee16a504cd3452f25
Adam Bergman <adam@fransvilhelm.com>
2019-05-23 11:23:14 +0200

    fix: Setup separate stage domains

---
+++ serverless.config.js
@@ -0,0 +1,27 @@
+module.exports = sls => {
+  if (process.env.AWS_CERTIFICATE_ARN == null) {
+    throw new Error('Environment variable AWS_CERTIFICATE_ARN is missing');
+  }
+
+  const stage = process.env.STAGE || process.env.NODE_ENV || 'development';
+  const cloudFrontDomain =
+    stage === 'production'
+      ? 'images.sjofartstidningen.se'
+      : `${stage}.images.sjofartstidningen.se`;
+
+  return {
+    defaultStage: stage,
+    defaultRegion: 'eu-north-1',
+    apigwBinary: {
+      types: ['image/*'],
+    },
+    apiCloudFront: {
+      domain: cloudFrontDomain,
+      certificate: process.env.AWS_CERTIFICATE_ARN,
+      compress: true,
+      cookies: 'none',
+      headers: ['Accept'],
+      querystring: 'all',
+    },
+  };
+};
